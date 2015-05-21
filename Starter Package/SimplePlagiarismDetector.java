22b8056d3e9c2abd798d2bfe6c975de03ff518fb
waseemx10 <waseemx10@gmail.com>
2015-05-21 17:44:35 +0300

    Added imports

--- Starter Package/SimplePlagiarismDetector.java
+++ Starter Package/SimplePlagiarismDetector.java
@@ -1,11 +1,13 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 public class SimplePlagiarismDetector{
